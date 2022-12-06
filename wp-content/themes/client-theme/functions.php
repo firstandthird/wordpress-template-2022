416f3ce4d1ff3b1b0f01006af520bac50211a31a
ApplianceJohn <thednbgremlin@gmail.com>
2022-12-06 14:07:12 -0500

    updated client theme defaults and meta

--- wp-content/themes/client-theme/functions.php
+++ wp-content/themes/client-theme/functions.php
@@ -11,7 +11,7 @@ function client_theme_scripts_styles() {
   );
   wp_enqueue_style(
     'client-theme',
-    get_stylesheet_uri(),
+    get_stylesheet_directory_uri() . '/css/app.css',
     array( $parenthandle ),
     $theme->get( 'Version' )
   );
@@ -21,5 +21,30 @@ function client_theme_scripts_styles() {
     array(),
     $theme->parent()->get( 'Version' ),
     true
+  );
+  wp_enqueue_script(
+    'client-theme',
+    get_stylesheet_directory_uri() . '/js/app.js',
+    array(),
+    $theme->get( 'Version' ),
+    true
+  );
+  add_editor_style( 'css/editor-style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'client_theme_fonts' );
+function client_theme_fonts() {
+  $theme = wp_get_theme();
+  wp_enqueue_style(
+    'font-catamaran',
+    'https://fonts.googleapis.com/css2?family=Catamaran:wght@400;700&display=swap',
+    array(),
+    '0.1.0'
+  );
+  wp_enqueue_style(
+    'font-outfit',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap',
+    array(),
+    '0.1.0'
   );
 }
