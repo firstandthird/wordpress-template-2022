23f0337a8c99b8215c3a1c0ebf1baa860291dc13
ApplianceJohn <thednbgremlin@gmail.com>
2022-12-22 12:22:55 -0500

    moved all CSS and JS files to client theme

--- wp-content/themes/client-theme/functions.php
+++ wp-content/themes/client-theme/functions.php
@@ -1,26 +1,12 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'client_theme_scripts_styles' );
 function client_theme_scripts_styles() {
-  $parenthandle = 'tailpress';
-  $theme        = wp_get_theme();
-  wp_enqueue_style(
-    $parenthandle,
-    get_template_directory_uri() . '/css/app.css',
-    array(),
-    $theme->parent()->get( 'Version' )
-  );
+  $theme = wp_get_theme();
   wp_enqueue_style(
     'client-theme',
     get_stylesheet_directory_uri() . '/css/app.css',
     array( $parenthandle ),
     $theme->get( 'Version' )
-  );
-  wp_enqueue_script(
-    $parenthandle,
-    get_template_directory_uri() . '/js/app.js',
-    array(),
-    $theme->parent()->get( 'Version' ),
-    true
   );
   wp_enqueue_script(
     'client-theme',
@@ -47,4 +33,9 @@ function client_theme_fonts() {
     array(),
     '0.1.0'
   );
+}
+
+add_action( 'init', 'register_acf_blocks' );
+function register_acf_blocks() {
+  register_block_type( __DIR__ . '/blocks/testimonial' );
 }
