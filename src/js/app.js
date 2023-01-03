fd6052364982489742d87803dfbf30ae0d46adc1
ApplianceJohn <thednbgremlin@gmail.com>
2023-01-03 17:49:08 -0500

    moved base themes to src

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
-/* Custom JS */
+// Navigation toggle
+window.addEventListener("load", function () {
+  let main_navigation = document.querySelector("#primary-menu");
+  document
+    .querySelector("#primary-menu-toggle")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      main_navigation.classList.toggle("hidden");
+    });
+});
