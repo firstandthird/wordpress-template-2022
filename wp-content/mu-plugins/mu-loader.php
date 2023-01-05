bb71bf680d430a3898736aea8a1782c8d837bc0a
ApplianceJohn <thednbgremlin@gmail.com>
2023-01-04 20:08:09 -0500

    added mu-plugin loader file

---
+++ wp-content/mu-plugins/mu-loader.php
@@ -0,0 +1,6 @@
+<?php // Add plugins here
+
+require WPMU_PLUGIN_DIR . '/acf-extended/acf-extended.php';
+require WPMU_PLUGIN_DIR . '/svg-support/svg-support.php';
+require WPMU_PLUGIN_DIR . '/timber-library/timber.php';
+require WPMU_PLUGIN_DIR . '/wordpress-seo/wp-seo.php';
