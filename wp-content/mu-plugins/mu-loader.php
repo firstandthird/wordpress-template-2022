d07ef8e1c4b14bf1a28505a606938b3041569cea
ApplianceJohn <thednbgremlin@gmail.com>
2023-01-05 16:53:08 -0500

    removed Timber and added ACF to mu-plugins

--- wp-content/mu-plugins/mu-loader.php
+++ wp-content/mu-plugins/mu-loader.php
@@ -1,6 +1,6 @@
 <?php // Add plugins here
 
+require WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR . '/acf-extended/acf-extended.php';
 require WPMU_PLUGIN_DIR . '/svg-support/svg-support.php';
-require WPMU_PLUGIN_DIR . '/timber-library/timber.php';
 require WPMU_PLUGIN_DIR . '/wordpress-seo/wp-seo.php';
