76807493c3b442af51dc1ee9bc664fa8cf03d9aa
ApplianceJohn <thednbgremlin@gmail.com>
2023-01-27 16:54:38 -0500

    added new Dandy Blocks version to Composer

--- wp-content/mu-plugins/mu-loader.php
+++ wp-content/mu-plugins/mu-loader.php
@@ -4,3 +4,4 @@ require WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php';
 require WPMU_PLUGIN_DIR . '/acf-extended/acf-extended.php';
 require WPMU_PLUGIN_DIR . '/svg-support/svg-support.php';
 require WPMU_PLUGIN_DIR . '/wordpress-seo/wp-seo.php';
+require WPMU_PLUGIN_DIR . '/dandy-blocks/ft-blocks-manager.php';
