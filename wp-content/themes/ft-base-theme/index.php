a3c12651a8b17fef421f3d5a01b223b5b858955b
Psiale <psialedev@gmail.com>
2025-01-27 13:48:53 -0600

    updating build:css-app and build:css-editor to run with new tailwind version

--- wp-content/themes/ft-base-theme/index.php
+++ wp-content/themes/ft-base-theme/index.php
@@ -2,17 +2,17 @@
 
 <div class="container mx-auto my-8">
   <?php if (is_archive()): ?>
-    <h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-8 max-w-content"><?= wp_kses_post(get_the_archive_title()); ?></h1>
+    <h1 class="entry-title text-2xl lg:text-5xl font-extrabold leading-tight mb-8 max-w-max"><?= wp_kses_post(get_the_archive_title()); ?></h1>
   <?php endif; ?>
-  <?php if ( have_posts() ) : ?>
+  <?php if (have_posts()) : ?>
     <?php
-    while ( have_posts() ) :
+    while (have_posts()) :
       the_post();
-      ?>
+    ?>
 
       <?php get_template_part('template-parts/content', get_post_format()); ?>
 
-      <?php endwhile; ?>
+    <?php endwhile; ?>
 
   <?php endif; ?>
 
